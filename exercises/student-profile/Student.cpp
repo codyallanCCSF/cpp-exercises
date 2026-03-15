2ef3b473640e870d05cb7f709b3aadb5cee3e6bd
codyallanCCSF <cgilber2@mail.ccsf.edu>
2026-03-15 11:34:32 -0700

    01 - Links header file and defines test function

---
+++ exercises/student-profile/Student.cpp
@@ -0,0 +1,10 @@
+// Link header file
+#include "Student.h"
+
+// Library for input/output
+#include <iostream>
+
+// Function definition
+void checkLink() {
+  std::cout << "Link establshed" << std::endl;
+}
