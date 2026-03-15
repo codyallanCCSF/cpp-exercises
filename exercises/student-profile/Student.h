92cdc73daa2d054573e62f549f0c7a8b23329d5c
codyallanCCSF <cgilber2@mail.ccsf.edu>
2026-03-15 11:30:01 -0700

    01 - Adds header guards and test function signature

---
+++ exercises/student-profile/Student.h
@@ -0,0 +1,12 @@
+// Open header guard
+#ifndef STUDENT_H
+#define STUDENT_H
+
+// String dependency
+#include <string>
+
+// Temp function to test workspace
+void checkLink();
+
+// Close header guard
+#endif
