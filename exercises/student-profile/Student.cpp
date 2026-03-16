62bf4109b8aa90c6ce6742bfe76993da8162cac6
codyallanCCSF <cgilber2@mail.ccsf.edu>
2026-03-16 11:44:04 -0700

    03 - Defines parameterized constructor and displayProfile method

--- exercises/student-profile/Student.cpp
+++ exercises/student-profile/Student.cpp
@@ -4,9 +4,41 @@
 // Library for input/output
 #include <iostream>
 
-// Scope Resolution
+// Default constructor
+// In main: Student testObject
 Student::Student() {
-  // Empty for testing
+  studentName = "Unknown";
+  date.day = 1;
+  date.month = 1;
+  date.year = 2026;
+}
+
+// Parameterized constructor
+// In main: Student myStudent("Joe", 16, 3, 2026)
+Student::Student(std::string name, int day, int month, int year) {
+  studentName = name;
+  date.day = day;
+  date.month = month;
+  date.year = year;
+}
+
+// Display method
+void Student::displayProfile() {
+
+  std::cout 
+    << "Student: " 
+    << studentName
+    << std::endl;
+
+  std::cout 
+    << "Enrolled: "
+    << date.month
+    << "/"
+    << date.day
+    << "/"
+    << date.year
+    << std::endl;
+
 }
 
 // Method definition
