ca2b275de8d7e4f52aacd2f8a02919ebcf5000c9
codyallanCCSF <codybizniz@proton.me>
2026-05-01 15:12:47 -0700

    03 - Adds list constructor and addBack() definition

--- exercises/binary_search/BST.cpp
+++ exercises/binary_search/BST.cpp
@@ -1,16 +1,49 @@
 #include "BST.h"
 #include <iostream>
 
-// Constructor
+
+// Constructors
+
+Node::Node(int val) {
+  data = val;
+  next = nullptr;
+  prev = nullptr;
+}
+
 LinkedList::LinkedList() {
   head = nullptr;
   tail = nullptr;
 }
 
+// Add a new node
+LinkedList::addBack(int val) {
+  Node* newNode = new Node(val);
+
+  if (head == nullptr) {
+    //For empty list, head is also tail
+    head = newNode;
+    tail = newNode;
+  } else {
+    // Update old tail's next to new node
+    tail->next = newNode;
+    // Assign new node's prev to current tail
+    newNode->prev = tail;
+    // Update tail to new node
+    tail = newNode;
+  }
+}
+
 LinkedList::~LinkedList() {
+
   
 }
 
 void LinkedList::helloWorld() {
   std::cout << "Hello from the BST.cpp file! Link successful." << std::endl;
+}
+
+void LinkedList::addBack(int val) {
+  Node* newNode = new Node(val);
+
+
 }
